f3d5c64f0e84d451897738f8defcfe914dfd50c1
alirezaMalekei <alireza.malekei72@gmail.com>
2026-02-15 22:43:03 +0330

    init test

---
+++ vue-client/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import {reactive} from "vue";
+import axios from "axios";
+
+const form = reactive({
+    email: 'admin@admin.com',
+    password: 'secret'
+
+});
+
+const login = async ()=>{
+    await axios.get('/sanctum/csrf-cookie');
+    const response = await axios.post('/login', form);
+    console.log(response);
+}
+</script>
+
+<template>
+    <form  @submit.prevent="login">
+        <input
+            v-model="form.email"
+            type="text" name="email" id="email" placeholder="Email">
+        <input
+            v-model="form.password"
+            type="password" name="password" id="password" placeholder="Password">
+        <button type="submit">login</button>
+    </form>
+</template>
+
+<style scoped></style>
---
+++ vue-client/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import axios from 'axios'
+axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;
+axios.defaults.baseURL='http://localhost:8000'
+createApp(App).mount('#app')
